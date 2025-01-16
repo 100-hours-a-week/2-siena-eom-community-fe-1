edab3ac140f5f661a9fef347be601c578bff20e0
siyeon <aeiou0302@icloud.com>
2025-01-16 14:52:35 +0900

    [feat] backspace 기능 수정

--- js/header.js
+++ js/header.js
@@ -1,3 +1,11 @@
+function goBack() {
+    if (document.referrer) {
+        window.history.back();
+    } else {
+        window.location.href = './post-list.html'; // 기본 이동 경로
+    }
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     const logoutLink = document.querySelector('.dropdown-menu a[href="./login.html"]');
     const headerTitle = document.getElementById("headerTitle");
@@ -56,7 +64,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             } else {
                 const result = await response.json();
                 console.error('로그아웃 실패:', result.message);
-                alert('로그아웃에 실패했습니다. 다시 시도해주세요.');
+                //alert('로그아웃에 실패했습니다. 다시 시도해주세요.');
             }
         } catch (error) {
             console.error('로그아웃 요청 중 오류:', error);
